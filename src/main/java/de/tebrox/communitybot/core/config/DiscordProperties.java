6fe9e8418c05ddf9b83617e9bd4b251d46b1457f
Tebrox <tebrox90@googlemail.com>
2026-03-08 09:01:21 +0100

    adding DiscordProperties for multiple DiscordBots

--- src/main/java/de/tebrox/communitybot/core/config/DiscordProperties.java
+++ src/main/java/de/tebrox/communitybot/core/config/DiscordProperties.java
@@ -1,12 +1,31 @@
 package de.tebrox.communitybot.core.config;
 
-import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
 
 @Validated
 @ConfigurationProperties(prefix = "discord")
-public record DiscordProperties(
-        @NotBlank(message = "Discord token missing: set DISCORD_TOKEN env var or discord.token in application.yml")
-        String token
-) {}
+public class DiscordProperties {
+
+        @Setter
+        @Getter
+        private BotProperties community = new BotProperties();
+
+        @Setter
+        @Getter
+        private BotProperties ticket = new BotProperties();
+
+
+    @Setter
+    @Getter
+    public static class BotProperties {
+            private boolean enabled = true;
+            private String token;
+
+            public boolean isConfigured() {
+                return token != null && !token.isBlank();
+            }
+    }
+}
